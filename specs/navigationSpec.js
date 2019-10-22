3e798237c3b0e903c90860d7ac9dd210f9dd8306
arunad <arunarani.ec@gmail.com>
2019-10-22 13:36:44 -0400

    navSpc:features & Benefit logo & Body updated

--- specs/navigationSpec.js
+++ specs/navigationSpec.js
@@ -4,13 +4,6 @@ browser.ignoreSynchronization = true;
 let loginEle = ssHelper.ele.login;
 let loginData = ssHelper.data;
 
-//Nav test
-function navTest(nav, navContent = "") {
-  element(By.css("a[href='" + nav + "']")).click();
-  browser.sleep(1000);
-  expect(element(By.css(nav + " " + navContent)).isDisplayed()).toBe(true);
-}
-
 describe("Navigation :", function() {
   beforeAll(() => {
     browser.get(ssHelper.ele.baseUrl);
@@ -22,13 +15,17 @@ describe("Navigation :", function() {
 
   it("Features", function() {
     navTest("#features");
+    featureLogoTest();
+    featureBodyTest();
   });
 
-  it("Benefits", function() {
+  xit("Benefits", function() {
     navTest("#benfits");
+    expect(element(By.css(".list-unstyled")).isDisplayed()).toBe(true);
+    expect(element(By.css('img[alt="iphone"]')).isDisplayed()).toBe(true);
   });
 
-  it("Pricing", function() {
+  xit("Pricing", function() {
     navTest("#pricing");
     priceCheck();
   });
@@ -38,25 +35,48 @@ describe("Navigation :", function() {
   });
 });
 
+//Nav test
+function navTest(nav, navContent = "") {
+  element(By.css("a[href='" + nav + "']")).click();
+  browser.sleep(1000);
+  expect(element(By.css(nav + " " + navContent)).isDisplayed()).toBe(true);
+}
+
 function priceCheck() {
   let arrayPrice = ["Bronze", "Silver", "Gold"];
   let list = element.all(By.css(".text-primary"));
 
   for (let i = 0; i < 3; i++) {
     if (expect(list.get(i).getText()).toBe(arrayPrice[i])) {
-      /* element(
+      element(
         By.cssContainingText(".btn.btn-primary", "Choose this Plan")
-      ).click(); */
-    if(i=0){
+      ).click();
+      /*  if(i=0){
       element(By.cssContainingText(".list-group-item", "  Up to 2 social media accounts"));
       element(By.cssContainingText(".btn.btn-primary", "Choose this Plan")
       ).click(); 
-    }
-    if(1=)
+    } */
+
       browser.sleep(5000);
       ssHelper.loginTest(loginEle, loginData);
       ssHelper.signOut();
       browser.navigate().back();
     }
+  }
+}
+
+function featureLogoTest() {
+  let arrayLogo = [".ti-face-smile", ".ti-receipt", ".ti-cup"];
+  for (i = 0; i < 3; i++) {
+    expect(element(By.css(arrayLogo[i])).isDisplayed()).toBe(true);
+  }
+}
+
+function featureBodyTest() {
+  let arrayBody = ["Understand", "Analyze", "Grow"];
+  for (i = 0; i < 3; i++) {
+    expect(
+      element(By.cssContainingText(".media h4", arrayBody[i])).isDisplayed()
+    ).toBe(true);
   }
 }
